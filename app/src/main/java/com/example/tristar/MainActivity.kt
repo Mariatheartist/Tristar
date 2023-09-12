8e96b75e6e3721497082c618e8fe8e915a6d261b
Gabriella Pierre <gabriellapierre1998@gmail.com>
2023-09-11 20:37:41 -0400

    Finished birthday Greeting

--- app/src/main/java/com/example/tristar/MainActivity.kt
+++ app/src/main/java/com/example/tristar/MainActivity.kt
@@ -3,16 +3,25 @@ package com.example.tristar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tristar.ui.theme.TristarTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,10 +31,48 @@ class MainActivity : ComponentActivity() {
             TristarTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Gabby")
+                    GreetingImage(message = getString(R.string.happy_birthday_gabby), from = getString(
+                                            R.string.birthday_signature_text))
                 }
             }
         }
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
+    val image = painterResource(R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F  //change the opacity of the bg img
+        )
+        GreetingText(message = message, from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp))
+    }
+}
+
+@Composable
+fun GreetingText(message: String,from: String,modifier: Modifier = Modifier){
+    // Create a column so that texts don't overlap
+    Column(verticalArrangement = Arrangement.Center, modifier = modifier){
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center,
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
     }
 }
 
@@ -41,9 +88,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BirthdayCardGreeting() {
     TristarTheme {
-        Greeting("Android")
+//        Greeting("Gabby")
+        // GreetingText(message = "Happy Birthday Gabby!", from="From Mommy")
+        GreetingImage(message = "Happy Birthday Gabby!", from = "From Mom")
     }
 }
 
